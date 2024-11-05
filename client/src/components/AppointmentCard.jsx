4594485cb7e0319127e13a12c6692d28672298f6
venetia <venetiafaber@gmail.com>
2024-11-05 11:11:37 -0500

    Adds clinic details in appointment card

--- client/src/components/AppointmentCard.jsx
+++ client/src/components/AppointmentCard.jsx
@@ -11,77 +11,64 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 export default function AppointmentCard({ appointment }) {
-  // const [appointments, setAppointments] = useState([]);
-  // const [loading, setLoading] = useState(true);
-  // const [error, setError] = useState(null);
 
-  // // fetches the appointments for the logged-in user
-  // useEffect(() => {
-  //   const fetchAppointments = async () => {
-  //       setLoading(true);
-  //     try {
-  //       const response = await axios.get(
-  //         `http://localhost:3002/api/patient/appointments?patientId=${patientId}`
-  //       );
-
-  //       console.log("Response data: ", response.data.appointments);
-  //       // console.log("Response patientId: ", response.data.appointments.patientId);
-
-  //       if (response.data && response.data.appointments) {
-  //         setAppointments(response.data.appointments);
-  //       } else {
-  //       //   console.error("No upcoming appointments found for this user:", patientId);
-  //           setError("No upcoming appointments found.");
-  //       }
-  //     } catch (error) {
-  //       // console.error("Error fetching appointment data: ", error);
-  //       setError("Error fetching appointment data.");
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   };
-
-  //   if (patientId) {
-  //     fetchAppointments();
-  //   }
-  // }, [patientId]);
-
-  // if (loading) {
-  //   return <p>Loading appointments...</p>
-  // }
-
-  // if (error) {
-  //   return <p>{ error }</p>
-  // }
-
-  // // displays a message if no appointments are found
-  // if (!appointments || appointments.length === 0) {
-  //   return <p>No upcoming appointments found..</p>;
-  // }
-
+  // defines state
+  const [showDetails, setShowDetails] = useState(false);
+ 
   if (!appointment) return null;
+
+  // clinic address
+  const { streetAddress, city, province, country, postCode } = appointment.clinicAddress || {};
+
+  // function to toggle details button
+  const handleDetailsToggle = () => {
+    setShowDetails(prevShowDetails => !prevShowDetails);
+  }
 
   return (
     <div className="appointment-card">
-      <h3>Appointment Details</h3>
-      <p>
-        <FontAwesomeIcon icon={faCalendarAlt} />
-        Date: {appointment.date}
-      </p>
-      <p>
-        <FontAwesomeIcon icon={faClock} />
-        Time: {appointment.time}
-      </p>
-      <p>
-        <FontAwesomeIcon icon={faUser} />
-        Visit Type: {appointment.visitType}
-      </p>
-      <p>Visit Mode: {appointment.visitMode}</p>
-      <p>Status: {appointment.status}</p>
-      <p>
-        <FontAwesomeIcon icon={faCommentDots} />
-        Comments: {appointment.comments}
-      </p>
+      <h3>
+        {appointment.clinicName}
+        <span className="visit-type">
+          {/* {appointment.visitType === "DoctorVisit" ? "At Clinic" : "At Lab"} */}
+          {appointment.status}
+          </span>
+      </h3>
+      <div className="appointment-card-items"> 
+
+        <div className="appointment-card-address">
+          {streetAddress && (
+            <p>
+              <FontAwesomeIcon icon={faLocationDot} className="icon"  />
+              {streetAddress}, {city}, {province}, {country}, {postCode}
+            </p>
+          )}
+        </div>
+        <div className="appointment-card-date-time">
+          <p><FontAwesomeIcon icon={faCalendarAlt} className="icon"/>{appointment.date}</p>  
+          <p><FontAwesomeIcon icon={faClock} className="icon"/>{appointment.time}</p>
+        </div>
+        
+        <div className="appointment-card-details-btn">
+          <button onClick={handleDetailsToggle}>
+            {showDetails ? "Hide Details" : "Details"}
+          </button>
+          
+        </div>
+        
+      </div>
+      
+      
+      {/* renders details when showDetails is true */}
+      {showDetails && (
+        <div className="appointment-card-p-container">
+          <p>Visit Mode: {appointment.visitMode}</p>
+          <p>Status: {appointment.status}</p>
+          <p><FontAwesomeIcon icon={faCommentDots} className="icon"/>
+            Comments: {appointment.comments}
+          </p>
+        </div>
+      )}
     </div>
   );
 }
