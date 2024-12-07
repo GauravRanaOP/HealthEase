504643bc7a750142f935a5f99fd4515317af466a
Yash Patel <yashjptl2611@gmail.com>
2024-12-07 18:09:46 -0500

    Modifies API to Update Booked Appointments Status

--- server/routes/ViewBookingsRoute.js
+++ server/routes/ViewBookingsRoute.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { getBookedAppointments } from "../controllers/ViewBookingsController.js";
+import { getBookedAppointments, updateAppointment } from "../controllers/ViewBookingsController.js";
 
 const ViewBookingsRoute = express.Router();
 
 //route to fetch booked appointments
 ViewBookingsRoute.get("/getBookedAppointments", getBookedAppointments);
+
+// Route to update an appointment's Test Status and Result
+ViewBookingsRoute.put("/:id", updateAppointment);
 
 export default ViewBookingsRoute;
