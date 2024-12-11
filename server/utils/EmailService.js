89ee8f8b6f62bef93ce6c80edd2042e1dedf9681
Gaurav Rana <gauravrana1998@gmail.com>
2024-12-11 18:55:26 -0500

    Email Service feature added while creating Clinic Admin

---
+++ server/utils/EmailService.js
@@ -0,0 +1,27 @@
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: 'nishuleuva786@gmail.com',
+    pass: 'borm ieva zgiv uvfs'
+  },
+});
+
+export const sendEmail = async (to, subject, html) => {
+  try {
+    await transporter.sendMail({
+      from: 'nishuleuva786@gmail.com',
+      to,
+      subject,
+      html
+    });
+    console.log('Email sent successfully');
+  } catch (error) {
+    console.error('Error sending email:', error);
+    throw error;
+  }
+};
+
